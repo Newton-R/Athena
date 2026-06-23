d3da27edc281fd761b3630529314c1c96c3b81af
Newton-R <newton4raul@gmail.com>
2026-06-23 19:35:10 +0100

    just link

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="h-screen flex w-screen items-center justify-center">
-      <Button className={"p-4 rounded-4xl"} size={"lg"}>
-        Get Started
-      </Button>
+      <Link href={"/docs/intro"}>
+        <Button variant={"link"} className={"p-4 rounded-4xl"} size={"lg"}>
+          Get Started
+        </Button>
+      </Link>
     </main>
   );
 }
